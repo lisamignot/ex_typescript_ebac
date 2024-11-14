03145ad61419a819fd41425922c7f3facdaa8b0c
Lisa Mignot <lisammignot@gmail.com>
2024-11-14 13:25:23 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+const dizOi = (nome) => `Olá, ${nome}!`;
+console.log(dizOi('Lisa'));
+const multiplicacao = (a, b) => a * b;
+console.log(multiplicacao(3, 3));
---
+++ main.ts
@@ -0,0 +1,6 @@
+const dizOi = (nome: string): string => `Olá, ${nome}!`;
+console.log(dizOi('Lisa'));
+
+
+const multiplicacao = (a: number, b: number): number => a * b;
+console.log(multiplicacao(3, 3));
